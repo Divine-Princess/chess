ac3d126836c87a8cb4add4979f2ee51f85cd1120
Divine-Princess <emsuet87@byu.edu>
2026-02-26 15:46:23 -0700

    Implement RegisterHandler

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,34 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.authDAO.AuthDAO;
+import dataaccess.authDAO.MemoryAuthDAO;
+import dataaccess.userDAO.MemoryUserDAO;
+import dataaccess.userDAO.UserDAO;
 import io.javalin.*;
+import io.javalin.http.Context;
+import server.handler.RegisterHandler;
+import service.UserService;
+
+import java.util.Map;
 
 public class Server {
+
+    private UserService userService;
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
 
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        userService = new UserService(userDAO,authDAO);
 
-        // Register your endpoints and exception handlers here.
-
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                         .post("/user", context -> new RegisterHandler().handle(userService, context))
+                         .exception(Exception.class, this::exceptionHandler)
+                         .delete("/db", context -> new);
     }
 
     public int run(int desiredPort) {
@@ -20,5 +38,20 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void exceptionHandler(Exception e, Context context) {
+        var body = new Gson().toJson(Map.of("message", String.format(e.getMessage())));
+
+        if (e instanceof BadRequestException) {
+            context.status(400);
+        }
+        else if (e instanceof AlreadyTakenException) {
+            context.status(403);
+        }
+        else {
+            context.status(500);
+        }
+        context.json(body);
     }
 }
---
+++ server/src/main/java/server/handler/RegisterHandler.java
@@ -0,0 +1,21 @@
+package server.handler;
+
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import model.request.RegisterRequest;
+import model.result.RegisterResult;
+import server.AlreadyTakenException;
+import server.BadRequestException;
+import service.UserService;
+
+import java.util.Map;
+
+public class RegisterHandler {
+    public void handle(UserService userService, Context context) {
+
+        RegisterRequest request = new Gson().fromJson(context.body(), RegisterRequest.class);
+        RegisterResult result = userService.register(request);
+
+        context.result(new Gson().toJson(result));
+    }
+}
