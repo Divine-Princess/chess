a814d24aeef0bd0e48b87864553bf86ea8ad9394
Divine-Princess <emsuet87@byu.edu>
2026-03-20 18:31:13 -0600

    implement help

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -2,6 +2,8 @@ package client;
 
 import serverfacade.ServerFacade;
 
+import java.util.Scanner;
+
 public class ChessClient {
 
     private final ServerFacade server;
@@ -17,6 +19,47 @@ public class ChessClient {
     }
 
     public void run() {
+        System.out.println("Welcome to CHESS 240!");
+        padding();
+        System.out.println(help());
 
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while (!userInput.equals("quit")) {
+            prompt();
+            String input = scanner.nextLine();
+
+
+        }
+
+    }
+
+    private void prompt() {
+        System.out.println("\n>>>");
+    }
+
+    private void padding() {
+        System.out.println("\n* * *\n");
+    }
+
+    private String help() {
+        if (state == State.LOGGEDOUT) {
+            return """
+                    * help -> List possible commands
+                    * register [USERNAME] [PASSWORD] [EMAIL] -> Create Account
+                    * login [USERNAME] [PASSWORD] -> Login as existing user
+                    * quit -> Quit CHESS 240
+                    """;
+        }
+        return """
+                * create [NAME] -> Make a new CHESS 240 game with name of choice
+                * list -> List all existing CHESS 240 games
+                * join [ID] [WHITE/BLACK] -> Join an existing CHESS 240 game
+                * observe [ID] -> View an existing CHESS 240 game
+                * logout -> Logout current user
+                * quit -> Quit CHESS 240
+                * help -> List possible commands
+                """;
     }
 }
